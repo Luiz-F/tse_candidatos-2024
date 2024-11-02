d7189cbb401a55bae3e56735e3f2360e8ca32016
Luiz Fernando Affonso <lf.affonso@gmail.com>
2024-11-02 18:25:30 -0300

    Query SQL

--- src/query.sql
+++ src/query.sql
@@ -13,11 +13,33 @@ with tb_cand as (
 		DS_OCUPACAO
     from tb_candidaturas
 ),
+
 tb_total_bens as(
 	select
 		SQ_CANDIDATO,
-		cast(replace(VR_BEM_CANDIDATO,',','.') as decimal(15,2))
+		sum(cast(VR_BEM_CANDIDATO as decimal(15,2))) TOTAL_BENS
 	from tb_bens
+GROUP BY 1
+),
+
+tb_info_compelta as (
+select 
+	tb_cand.*,
+	COALESCE (tb_total_bens.TOTAL_BENS,666) TotalBens
+from tb_total_bens 
+JOIN tb_cand on tb_cand.SQ_CANDIDATO = tb_total_bens.SQ_CANDIDATO
 )
 
-select * from tb_total_bens;
+select
+	sg_partido,
+	nm_partido,
+	concat(round(avg(case when ds_genero = 'FEMININO' then 1 else 0 end ) *100,2),'%') 'TxMulheres',
+	concat(round(avg(case when ds_genero = 'MASCULINO' then 1 else 0 end ) *100,2),'%')  'TxHomens',
+	avg(case when ds_cor_raca = 'PRETA' then 1 else 0 end) txNegros,
+  avg(case when ds_cor_raca = 'BRANCA' then 1 else 0 end) txBrancos,
+  avg(case when ds_cor_raca = 'PARDA' then 1 else 0 end) txPardos
+from tb_info_compelta
+group by 1,2
+order by 3 desc
+
+
